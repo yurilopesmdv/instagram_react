d52951de87a652ebd47da8e1de477693a7d52ac4
yurilopesm <yurilopesm@hotmail.com>
2023-02-07 14:54:40 -0300

    versao com components linkados e renderizando tudo na tela

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar"
+import Corpo from "./Corpo"
+export default function App() {
+    return (
+    <div class="conteudo">
+      <NavBar />  
+      <Corpo />
+    </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import App from './components/App';
+
+const app = App();
+
+ReactDOM.render(app, document.querySelector(".root"));
